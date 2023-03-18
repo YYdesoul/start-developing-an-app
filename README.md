83b405638e2bc03e6e34d0c9ae49275239eb33aa
YYdesoul <yanyusoul@gmail.com>
2023-03-18 11:12:17 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# 后端项目
+
+编程语言：Java, Golang, C, C++
+开发人员发现在开发中经常遇到同样的场景需要进行重复开发，这个时候开发人员就将这些重复代码整理为代码库，最终将多个代码库整合为一个框架
+
+框架：
+    
+- 开发框
+- ORM框架（操作数据库）
+
+Java 常用开发框架：SpringBoot
+ORM框架：Spring Data JPA, MyBatis, MyBatis Plus
---
+++ start-spingboot/src/main/java/com/jizhi/startspingboot/StartSpingbootApplication.java
@@ -0,0 +1,13 @@
+package com.jizhi.startspingboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartSpingbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartSpingbootApplication.class, args);
+    }
+
+}
