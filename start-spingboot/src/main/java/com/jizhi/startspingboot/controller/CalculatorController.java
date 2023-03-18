df960d4bfea24b41bb61eb4d5048b3a83e069c87
YYdesoul <yanyusoul@gmail.com>
2023-03-18 11:55:53 +0100

    created calculatorController and implemented /sub endpoint

---
+++ start-spingboot/src/main/java/com/jizhi/startspingboot/controller/CalculatorController.java
@@ -0,0 +1,28 @@
+package com.jizhi.startspingboot.controller;
+
+import com.jizhi.startspingboot.service.CalculatorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import javax.websocket.server.PathParam;
+
+@RestController
+public class CalculatorController {
+
+    @Autowired
+    private CalculatorService calculatorService;
+
+    @GetMapping("/add")
+    public int add(@PathParam("num1") int num1, @PathParam("num2") int num2) {
+        int result = calculatorService.add(num1, num2);
+        return result;
+    }
+
+    @GetMapping("/sub")
+    public int sub(@PathParam("num1") int num1, @PathParam("num2") int num2) {
+        int sub = calculatorService.sub(num1, num2);
+        return sub;
+    }
+}
+
--- start-spingboot/src/main/java/com/jizhi/startspingboot/controller/HelloController.java
+++ start-spingboot/src/main/java/com/jizhi/startspingboot/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.jizhi.startspingboot;
+package com.jizhi.startspingboot.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
