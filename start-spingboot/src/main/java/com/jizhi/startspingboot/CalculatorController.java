3072ef66a1c4230495d7bbbec3e2a103283603cd
YYdesoul <yanyusoul@gmail.com>
2023-03-18 11:59:37 +0100

    created calculatorController and implemented /div endpoint

--- start-spingboot/src/main/java/com/jizhi/startspingboot/CalculatorController.java
+++ start-spingboot/src/main/java/com/jizhi/startspingboot/CalculatorController.java
@@ -12,6 +12,14 @@ public class CalculatorController {
     public int add(@PathParam("num1") int num1, @PathParam("num2") int num2) {
         int sum = num1 + num2;
         return sum;
+    }
 
+    @GetMapping("/div")
+    public String div(@PathParam("num1") int num1, @PathParam("num2") int num2) {
+        if (num2 == 0) {
+            return "num2 can't be 0";
+        }
+        int div = num1 / num2;
+        return String.valueOf(div);
     }
 }
